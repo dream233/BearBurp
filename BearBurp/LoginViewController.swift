dd010f78fc090914dfd7c3876500c335e6082432
QY-W <qiyuan.w@wustl.edu>
2022-11-11 23:11:09 -0600

    add alerts to user message

--- BearBurp/LoginViewController.swift
+++ BearBurp/LoginViewController.swift
@@ -30,6 +30,15 @@ class LoginViewController: UIViewController {
         
         // 成功会返回username，失败会返回错误信息，下同
         print(theData?.message)
+        
+        // 失败展示alert
+        if let loginIndicator = theData?.success, let loginMSG = theData?.message {
+            if (!loginIndicator){
+                showAlert(alertText: "Login Error", alertMessage: loginMSG)
+            }else{
+                // hide login UI and show logged in UI
+            }
+        }
     }
     @IBAction func click_register(_ sender: Any) {
         var url:URL?
@@ -39,5 +48,23 @@ class LoginViewController: UIViewController {
         let data = try! Data(contentsOf: url!)
         theData = try! JSONDecoder().decode(Message.self,from:data)
         print(theData?.message)
+        
+        if let regIndicator = theData?.success, let regMSG = theData?.message {
+            if (!regIndicator){
+                showAlert(alertText: "Register Error", alertMessage: regMSG)
+            }else{
+                showAlert(alertText: "Success", alertMessage: "You have registered a account, please login.")
+            }
+        }
+    }
+}
+
+extension UIViewController {
+//Show a basic alert
+    func showAlert(alertText : String, alertMessage : String) {
+        let alert = UIAlertController(title: alertText, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        //Add more actions as you see fit
+        self.present(alert, animated: true, completion: nil)
     }
 }
